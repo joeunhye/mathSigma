c83ea545c3a29036ce0c89a1032ea48e6b0aaf91
joeunhye <masimel33@gmail.com>
2023-01-27 22:34:45 +0900

    gsap animation 수정

--- js/gsap-ani.js
+++ js/gsap-ani.js
@@ -1,64 +1,64 @@
 gsap.registerPlugin(ScrollTrigger);
 
 //Section Gsap Common Control
-const anySectionEls = gsap.utils.toArray('section');
-anySectionEls.forEach((el,i)=>{
-    ScrollTrigger.create({
-        trigger:el,
-        toggleClass:'active',
-        // start:"top 90%",
-        // end:"bottom 10%",
-        start: "center center",
+const anySectionEls = gsap.utils.toArray("section");
+anySectionEls.forEach((el, i) => {
+	ScrollTrigger.create({
+		trigger: el,
+		toggleClass: "active",
+		// start:"top 90%",
+		// end:"bottom 10%",
+		start: "center center",
 		end: "bottom top",
-        // markers: true,
-        // scrub:true,
-    });
+		// markers: true,
+		// scrub:true,
+	});
 });
 
 //Section2 Gsap Control
 const anim = gsap.to(".sec02 .cont-sub-text, .sec02 h3, .sec02 .cont-text, .sec02 .sigma-items li", {
-    y: 0,
-    autoAlpha: 1,
-    duration: 1,
-    stagger: 0.25,
+	y: 0,
+	autoAlpha: 1,
+	duration: 1,
+	stagger: 0.25,
 });
 
 ScrollTrigger.create({
-    trigger: ".sec02",
-    animation: anim,
-    toggleActions: "restart pause restart pause",
-    // markers: true,
+	trigger: ".sec02",
+	animation: anim,
+	toggleActions: "restart pause restart pause",
+	// markers: true,
 });
-
 
 //Section3 Gsap Control
 gsap.timeline({
 	scrollTrigger: {
 		trigger: ".sec03",
-		start: "center center",
-		end: "bottom top",
-		scrub: 1,
-		pin: true,
+		start: "top bottom",
+		end: "top top",
+		scrub: 3,
+		pin: false,
+		markers: true,
 	},
 })
-.from(".sec03 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec03 .img-wrap img", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-});
+	.from(".sec03 .anyText", {
+		y: innerHeight * 1,
+		stagger: {
+			amount: 0.5,
+		},
+	})
+	.from(".sec03 .img-wrap img", {
+		y: innerHeight * 1,
+		stagger: {
+			amount: 0.5,
+		},
+	});
 
 //Section4 Gsap Control
 const lineAny = gsap.to(".sec04 .line-bg", {
-    y: -20,
-    autoAlpha: 1,
-    duration: 1,
+	y: -20,
+	autoAlpha: 1,
+	duration: 1,
 });
 
 gsap.timeline({
@@ -67,27 +67,26 @@ gsap.timeline({
 		start: "center center",
 		end: "bottom top",
 		scrub: 1,
-        toggleClass:'on',
+		toggleClass: "on",
 		pin: true,
-        
 	},
 })
-// .from(".sec04 .line-bg", {
-//     y: innerHeight * 1,
-//     opacity: 0,
-// })
-.from(".sec04 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec04 .img-wrap img", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-});
+	// .from(".sec04 .line-bg", {
+	//     y: innerHeight * 1,
+	//     opacity: 0,
+	// })
+	.from(".sec04 .anyText", {
+		y: innerHeight * 1,
+		stagger: {
+			amount: 0.5,
+		},
+	})
+	.from(".sec04 .img-wrap img", {
+		y: innerHeight * 1,
+		stagger: {
+			amount: 0.5,
+		},
+	});
 
 //Section5 Gsap Control
 gsap.timeline({
@@ -99,15 +98,16 @@ gsap.timeline({
 		pin: true,
 	},
 })
-.from(".sec05.dark", {
-    opacity: 0,
-})
-.from(".sec05 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec05 .img-wrap", {
-    y: innerHeight * 1,
-});
+	.from(".sec05.dark", {
+		opacity: 0,
+	})
+	.from(".sec05 .anyText", {
+		y: innerHeight * 1,
+		stagger: {
+			amount: 0.5,
+		},
+	})
+	.from(".sec05 .img-wrap", {
+		opacity: 0,
+		y: innerHeight * 1,
+	});
