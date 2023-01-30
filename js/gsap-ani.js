e888c94eb9c8c2d7066a6fa5c5b45a91659c4d49
eunhye919 <eunhye919@vtex.co.kr>
2023-01-30 10:02:14 +0900

    gsap

--- js/gsap-ani.js
+++ js/gsap-ani.js
@@ -15,99 +15,115 @@ anySectionEls.forEach((el,i)=>{
     });
 });
 
-//Section2 Gsap Control
-const anim = gsap.to(".sec02 .cont-sub-text, .sec02 h3, .sec02 .cont-text, .sec02 .sigma-items li", {
-    y: 0,
-    autoAlpha: 1,
-    duration: 1,
-    stagger: 0.25,
-});
+// ScrollTrigger.saveStyles(".mobile, .desktop");
 
-ScrollTrigger.create({
-    trigger: ".sec02",
-    animation: anim,
-    toggleActions: "restart pause restart pause",
-    // markers: true,
-});
+ScrollTrigger.matchMedia({
+	
+	"(min-width: 1024px)": function() {
+        //Section3 Gsap Control
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".sec03",
+                start: "50% 50%",
+                end: "bottom top",
+                scrub: 1,
+                pin: true,
+                markers: true,
+            },
+        })
+        .from(".sec03 .anyText", {
+            y: innerHeight * 1,
+            opacity: 0,
+            stagger: {
+                amount: 0.5,
+            },
+        })
+        .from(".sec03 .img-wrap img", {
+            y: innerHeight * 1,
+            stagger: {
+                amount: 0.5,
+            },
+        });
 
+        //Section4 Gsap Control
+        const lineAny = gsap.to(".sec04 .line-bg", {
+            y: -20,
+            autoAlpha: 1,
+            duration: 1,
+        });
 
-//Section3 Gsap Control
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".sec03",
-		start: "center center",
-		end: "bottom top",
-		scrub: 1,
-		pin: true,
-	},
-})
-.from(".sec03 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec03 .img-wrap img", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-});
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".sec04",
+                start: "center center",
+                end: "bottom top",
+                scrub: 1,
+                toggleClass:'on',
+                pin: true,
+                
+            },
+        })
+        // .from(".sec04 .line-bg", {
+        //     y: innerHeight * 1,
+        //     opacity: 0,
+        // })
+        .from(".sec04 .anyText", {
+            y: innerHeight * 1,
+            stagger: {
+                amount: 0.5,
+            },
+        })
+        .from(".sec04 .img-wrap img", {
+            y: innerHeight * 1,
+            stagger: {
+                amount: 0.5,
+            },
+        });
 
-//Section4 Gsap Control
-const lineAny = gsap.to(".sec04 .line-bg", {
-    y: -20,
-    autoAlpha: 1,
-    duration: 1,
-});
-
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".sec04",
-		start: "center center",
-		end: "bottom top",
-		scrub: 1,
-        toggleClass:'on',
-		pin: true,
+        //Section5 Gsap Control
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".sec05",
+                start: "center center",
+                end: "bottom top",
+                scrub: 1,
+                pin: true,
+            },
+        })
+        .from(".sec05.dark", {
+            opacity: 0,
+        })
+        .from(".sec05 .anyText", {
+            y: innerHeight * 1,
+            stagger: {
+                amount: 0.5,
+            },
+        })
+        .from(".sec05 .img-wrap", {
+            y: innerHeight * 1,
+        });
+  }, 
+  
+	"(max-width: 1023px)": function() {
         
-	},
-})
-// .from(".sec04 .line-bg", {
-//     y: innerHeight * 1,
-//     opacity: 0,
-// })
-.from(".sec04 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec04 .img-wrap img", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
+  }, 
+  
+	"all": function() {
+        //Section2 Gsap Control
+        const anim = gsap.to(".sec02 .cont-sub-text, .sec02 h3, .sec02 .cont-text, .sec02 .sigma-items li", {
+            y: 0,
+            autoAlpha: 1,
+            duration: 1,
+            stagger: 0.25,
+        });
+
+        ScrollTrigger.create({
+            trigger: ".sec02",
+            animation: anim,
+            toggleActions: "restart pause restart pause",
+            // markers: true,
+        });
+  }
+  
 });
 
-//Section5 Gsap Control
-gsap.timeline({
-	scrollTrigger: {
-		trigger: ".sec05",
-		start: "center center",
-		end: "bottom top",
-		scrub: 1,
-		pin: true,
-	},
-})
-.from(".sec05.dark", {
-    opacity: 0,
-})
-.from(".sec05 .anyText", {
-    y: innerHeight * 1,
-    stagger: {
-        amount: 0.5,
-    },
-})
-.from(".sec05 .img-wrap", {
-    y: innerHeight * 1,
-});
